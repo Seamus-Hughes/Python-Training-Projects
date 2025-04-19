7659df36570e87f19b1c2d50f3b5dc3659a946f3
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-19 22:48:22 +0100

    Formatting Dates (strftime)

--- journal_template.py
+++ journal_template.py
@@ -3,13 +3,13 @@
 # Purpose: Template for my weekly Lifestream posts.
 
 #----Imported Module----
-import datetime # Correctly imported!
+import datetime
 
 #----Variables----
 # ... (previous variables are fine) ...
 page_title = "# Lifestream Week 2389"
 author = "*by Seamus Hughes*"
-day_title = "#### Thursday" # We'll link this to the actual date later!
+day_title = "#### Thursday" # We'll update this using the formatted date soon!
 
 # markdown template of post
 # NOTE, number in [] is dynamic and changed for each day.
@@ -20,13 +20,20 @@ post_template = """
 [1]: note"""
 
 # ----Date----
-# get todays daye object l # (Minor typo: date)
-todays_date = datetime.date.today() # Correctly gets the date object!
-print(todays_date) # Correctly prints it!
+# get todays date object
+todays_date = datetime.date.today()
+print(todays_date) # Prints the original date object (e.g., 2025-04-07)
 
+# formated date in a string
+# NOTE prefered format is 'Monday 7th April 2025'
+# please facilitate adding "st" "nd" and "th" as a future part of this tutorial.
+formated_date = todays_date.strftime("%A, %B, %-d, %Y") # Correct use of strftime!
+print(formated_date) # Prints the formatted string!
+print("")
 # ----Combine variables----
-# NOTE f""" .... """ multi lines with {strings} dosen't work in this app # (Minor typos: strings, doesn't)
+# NOTE f""" .... """ multi lines with {strings} doesn't work in this app # (Minor typos: strings, doesn't)
 full_day_entry = f"{day_title}\n{post_template}"
 
 # combine all.
 print(f"{page_title}\n\n{author}\n\n{full_day_entry}")
+
