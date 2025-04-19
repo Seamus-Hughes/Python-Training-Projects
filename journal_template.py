93d006bebd8968d54deac76786cd9492d4b4cdf3
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-19 22:36:11 +0100

    Basic variables

--- journal_template.py
+++ journal_template.py
@@ -1,10 +1,16 @@
-# Autor: Seamus Hughes
+# Author: Seamus Hughes
 # Date: 4th April 2025
 # Purpose: Demonstrates printing message and comments.
 
+#assign name and hobby
+my_name = James
+my_hobby = Reading
+
 # To display name.
-print("Hello, my name is James")
+print("Hello, my name is:")
+print(my_name)
 # To display hobby.
-print("My favourite hobby is reading.")
+print("My favourite hobby is:")
+print(my_hobby)
 # Don't show my age
 # print("My age is 45")
