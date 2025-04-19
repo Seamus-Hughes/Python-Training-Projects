964ce6f21c32682a64796e2d518301c533498b73
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-19 23:12:20 +0100

    Create Functions

--- journal_template.py
+++ journal_template.py
@@ -1,63 +1,48 @@
 # Author: Seamus Hughes
-# Date: 5th April 2025
+# Date: 8th April 2025
 # Purpose: Template for my weekly Lifestream posts.
 
-#----Imported Module----
+#----Imported Modules----
 import datetime 
 
-#----Variables----
-# NOTE week number is dynamic and will change with each week. 
-# NOTE day_title will be dynamic 
-# NOTE week starts in Thursday for this project.
-# NOTE full date to be added. 
-page_title = "# Lifestream Week 2389"
-author = "*by Seamus Hughes*"
-day_title = "#### Thursday"
+#----Fuctions-----
 
-# markdown template of post 
-# NOTE, number in [] is dynamic and changed for each day. 
-post_template = """
-> quote[1]
-> -- attribute
-
-[1]: note"""
-
-# ----Date----
-# create date objects 
-todays_date = datetime.date.today()
-delta_1_day = datetime.timedelta(days=1)
-
-# Loop to create 7 days. 
-# counting through the days of the week.
-for i in range(7):
-	# iterate through 7 days
-	delta_day = datetime.timedelta(days=i)
-	# create datetime object for eaxh of 7 days 
-	# 1st day always today as i = 0
-	day_of_week = todays_date + delta_day
-	# Displays dates
-	print(day_of_week)
-print("")
-
-# Loop to create 7 days. 
-# counting through the days of the week.
-for i in range(7):
-	# iterate through 7 days (formatted)
-	delta_day = datetime.timedelta(days=i)
-	# create datetime object for eaxh of 7 days 
-	# 1st day always today as i = 0
-	day_of_week = todays_date + delta_day
-	# Formatted date in a string
+def create_formatted_date(date_object):
+	'''Formatting date into a nice looking string'''
 	# NOTE prefered format is 'Monday 7th April 2025'
 	# please facilitate adding "st" "nd" and "th" as a future part of this tutorial. 
-	formatted_day_of_week = day_of_week.strftime("%A, %B, %-d, %Y")
-	# Displays dates
-	print(formatted_day_of_week)
-print("")
+	# NOTE %-d used to get rid of leading zero might not work in some instances. 
+	formatted_date = date_object.strftime("%A %-d %B %Y") # Output = Monday 7 April 2025
+	return formatted_date
+	
 
-# ----Combine variables----
-# NOTE f""" .... """ multi lines with {strings} doesn't work in this app
-full_day_entry = f"{day_title}\n{post_template}"
+def create_daily_entry(entry_num):
+	'''Using range number given, will create template for daily lifelog post'''
+	# ----Date----
+	# create todays date 
+	todays_date = datetime.date.today()
+	# create datetime object for each of 7 days
+	# 1st day always today as entry_num = 0
+	delta_day = datetime.timedelta(days=entry_num)
+	day_of_week = todays_date + delta_day
+	formatted_day_of_week = create_formatted_date(day_of_week)
+	
+	# -----markdown template ------
+	# NOTE f""" .... """ multi lines with {strings} doesn't work in this app
+	day_entry = f"#### {formatted_day_of_week}\n\n> quote[^{entry_num+1}]\n> --attribute\n\n[^{entry_num+1}]: note"
+	return day_entry
+	
+#----Variables----
+# NOTE page_title is dynamic as week num will change. 
+# NOTE week starts in Thursday for this project.
+page_title = "# Lifestream Week 2389"
+author = "*by Seamus Hughes*"
 
-# combine all.
-print(f"{page_title}\n\n{author}\n\n{full_day_entry}")
+#-----Main Loop------
+
+# Create page header
+print(f"{page_title}\n\n{author}\n\n")
+# Create template post for each 7 days. 
+for i in range(7):
+	print(create_daily_entry(i))
+	print("")
