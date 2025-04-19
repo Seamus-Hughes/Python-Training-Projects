cba307a30c0a0c2727e72c78bee093879e6f6f92
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-19 22:34:00 +0100

    comments

--- journal_template.py
+++ journal_template.py
@@ -1,2 +1,10 @@
-print("My name is James")
-print(" My favourite hobby is reading.")
+# Autor: Seamus Hughes
+# Date: 4th April 2025
+# Purpose: Demonstrates printing message and comments.
+
+# To display name.
+print("Hello, my name is James")
+# To display hobby.
+print("My favourite hobby is reading.")
+# Don't show my age
+# print("My age is 45")
