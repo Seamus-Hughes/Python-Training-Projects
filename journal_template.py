1cdd2bbac65a39c3d3ed1e7f3be2bf63e623dc03
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-19 22:43:39 +0100

    f string for formatting

--- journal_template.py
+++ journal_template.py
@@ -2,23 +2,26 @@
 # Date: 5th April 2025
 # Purpose: Template for my weekly Lifestream posts.
 
-# Title of each page
-# NOTE week no is dynamic and will change with each week.
-page_title = "# Lifestream Week 2389" + "\n" + "\n"
-
-# Author - same every time.
-author = "*by Seamus Hughes*"
-
-# Day title will be dynamic
+#----Variables----
+# NOTE week number is dynamic and will change with each week.
+# NOTE day_title will be dynamic
 # NOTE week starts in Thursday for this project.
 # NOTE full date to be added.
+page_title = "# Lifestream Week 2389"
+author = "*by Seamus Hughes*"
 day_title = "#### Thursday"
 
 # markdown template of post
-# NOTE, number in [] is dynakic and chabged for each day.
-post_template = """> quote[1]\n> -- atribute\n\n[1]: note"""
+# NOTE, number in [] is dynamic and changed for each day.
+post_template = """
+> quote[1]
+> -- atribute
 
-print(page_title)
-print(author)
-print("\n")
-print(day_title + "\n" + post_template)
+[1]: note"""
+
+# ----Combine variables----
+# NOTE f""" .... """ muti lines with {steings} dosn't work in this app
+full_day_entry = f"{day_title}\n{post_template}"
+
+# combine all.
+print(f"{page_title}\n\n{author}\n\n{full_day_entry}")
