15e5a56da1ef35ca4d7090cc069d2a8832689c6d
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-19 23:02:47 +0100

    Loops for Repetition (range)

--- journal_template.py
+++ journal_template.py
@@ -31,21 +31,26 @@ delta_1_day = datetime.timedelta(days=1)
 yesterdays_date = todays_date - delta_1_day
 tomorrows_date = todays_date + delta_1_day
 
-# Dispay dates
+# Display dates
 
-print(f"Yeaterdays date: {yesterdays_date}")
+print(f"Yesterday's' date: {yesterdays_date}")
 print(f"Todays date: {todays_date}")
 print(f"Tomorrows date: {tomorrows_date}")
 print("")
-# formated date in a string
+# Formatted date in a string
 # NOTE prefered format is 'Monday 7th April 2025'
 # please facilitate adding "st" "nd" and "th" as a future part of this tutorial. 
-formated_yesterday = yesterdays_date.strftime("%A, %B, %-d, %Y")
-formated_today = todays_date.strftime("%A, %B, %-d, %Y")
-formated_tomorrow = tomorrows_date.strftime("%A, %B, %-d, %Y")
-print(formated_yesterday)
-print(formated_today)
-print(formated_tomorrow)
+formatted_yesterday = yesterdays_date.strftime("%A, %B, %-d, %Y")
+formatted_today = todays_date.strftime("%A, %B, %-d, %Y")
+formatted_tomorrow = tomorrows_date.strftime("%A, %B, %-d, %Y")
+print(formatted_yesterday)
+print(formatted_today)
+print(formatted_tomorrow)
+print("")
+
+# counting through the days of the week.
+for i in range(7):
+	print(i)
 print("")
 
 # ----Combine variables----
