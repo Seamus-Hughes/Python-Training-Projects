4fc43d106739a7e94869681b3258bfb1d233e139
Seamus-Hughes <188171896+Seamus-Hughes@users.noreply.github.com>
2025-04-22 17:33:51 +0100

    Moved weeks alive into function.

--- journal_template.py
+++ journal_template.py
@@ -10,11 +10,10 @@ import datetime
 def create_formatted_date(date_object):
 	'''Formatting date into a nice looking string'''
 	# NOTE prefered format is 'Monday 7th April 2025'
-	# please facilitate adding "st" "nd" and "th" as a future part of this tutorial. 
-	# NOTE %-d used to get rid of leading zero might not work in some instances. 
+	# please facilitate adding "st" "nd" and "th" as a future part of this tutorial.
+	# NOTE %-d used to get rid of leading zero might not work in some instances.
 	formatted_date = date_object.strftime("%A %-d %B %Y") # Output = Monday 7 April 2025
 	return formatted_date
-	
 
 def create_daily_entry(entry_num):
 	'''Using range number given, will create template for daily lifelog post'''
@@ -31,34 +30,33 @@ def create_daily_entry(entry_num):
 	# NOTE f""" .... """ multi lines with {strings} doesn't work in this app
 	day_entry = f"#### {formatted_day_of_week}\n\n> quote[^{entry_num+1}]\n> --attribute\n\n[^{entry_num+1}]: note"
 	return day_entry
+
+def weeks_alive(birthday):
+	'''Calculating how mamy weeks someone has been alive. NOTE birhday format need to be dd/mm/yyyy'''
+	format_code = "%d/%m/%Y" # MUST match string format!
+	today = datetime.datetime.today().date()
+	# parse the string into a datetime object. 
+	birth_date = datetime.datetime.strptime(birthday,format_code).date()
+	# Date Maths
+	date_difference = today - birth_date
+	days = date_difference.days
+	weeks = days // 7
+	return weeks
 	
 #----Variables----
-# NOTE page_title is dynamic as week num will change. 
-# NOTE week starts in Thursday for this project.
-page_title = "# Lifestream Week 2389"
-author = "*by Seamus Hughes*"
-
-#-----Weeks Alive-----
 # Fixed. birth date as personal project. 
 birthday = "28/06/1979"
-format_code = "%d/%m/%Y" # MUST match string format!
-today = datetime.datetime.today().date()
-# parse the string into a datetime object. 
-birth_date = datetime.datetime.strptime(birthday,format_code).date()
-# Date Maths
-date_difference = today - birth_date
-days = date_difference.days
-weeks = days // 7
+page_title = f"Lifestream Week {weeks_alive(birthday)}"
+author = "*by Seamus Hughes*"
 
+format_code = "%d/%m/%Y" # MUST match string format!
+birth_date = datetime.datetime.strptime(birthday,format_code).date()
 # Get the day of the week. (0 = Mon to 6 = Sun)
 day_number =  birth_date.weekday()
-
-# Output 
-print(f"Lifestream Week {weeks}")
 print(f"Weekday number (0 = Mon to 6 = Sun): {day_number}")
+# NOTE week starts in Thursday for this project.
 # Will print number. use logic later to convert to Mon, Tues....
 
-'''
 #-----Main Loop------
 
 # Create page header
@@ -67,4 +65,3 @@ print(f"{page_title}\n\n{author}\n\n")
 for i in range(7):
 	print(create_daily_entry(i))
 	print("")
-'''
