9789c7bc9fcbe8488fa83cf0fd7cd81ef2674630
Seamus Hughes <seamushughes10@gmail.com>
2025-09-14 20:01:53 +0100

    3.0 Finding File Details (like File Date, File Type, Size)
    Example Project: "File Inspector"
    This project will inspect a file (we'll use my_note.txt that our
    "Simple Note Taker" example might have created) and display its size,
    last modification date, creation date, and its extension.
    
    For example, you might want to:
    • Check how large a file is (e.g., before trying to email it or to
    monitor disk space).
    • Find out when a file was last modified (e.g., to see if a log file
    has been updated recently).
    • Determine the "type" of a file (e.g., is it a .txt file, a .jpg
    image, etc.?).
    Python provides tools for this, primarily through its built-in os
    module, which provides a way of using operating system dependent
    functionality.
    Introducing the os module and datetime module:
    
    The os module:
    The os module in Python gives you functions to interact with the
    operating system. A particularly useful part of it for file details is
    the os.path submodule.

--- daily_log_creator.py
+++ daily_log_creator.py
@@ -1,9 +1,14 @@
 # Author: Seamus Hughes
-# Date: 31st May 2025
-# Purpose: Creating a daily log using the append file function.
+# Date: 9th September  2025
+# Purpose: Creating a daily log using the append file function. with added log file check
 
 #----Imported Modules----
 
+# Interacting with operating system including file managment
+import os
+
+# for fomating date and time. 
+import datetime 
 
 #----Functions-----
 
@@ -14,7 +19,34 @@
 file_path = 'daily_log.txt' 
 
 #-----Main Logic------
+print("\n")
+print(f"----Log File Inspector----")
+print(f"Analyzing file: {file_path}")
+print("\n")
+
+# Check if file exists
+if os.path.exists(file_path):
+	# print file path 
+	print (f"File path: {file_path}")
+	# Get file size
+	file_size = os.path.getsize(file_path)
+	print(f"size: {file_size} bytes")
+	mod_timestamp = os.path.getmtime(file_path)
+	# convert time stamp into datetime object. 
+	mod_datetime = datetime.datetime.fromtimestamp(mod_timestamp)
+	# Format timestamp into readable time. 
+	format_timestamp = mod_datetime.strftime("%d.%m.%y %H:%M:%S")
+	print(f"modified: {format_timestamp}")
+	# get file type
+	filename_root, file_extention = os.path.splitext(file_path)
+	print(f"Extention: {file_extention}")
+	print("\n")
+# Returns error message if file does not exist. 
+else:
+	print(f"Error {file_path} not found")
+
 print("---Adding a note to your daily log.----")
+print("\n")
 
 # Requesting note input from user. 
 daily_note = input("What would you like to add? ")
