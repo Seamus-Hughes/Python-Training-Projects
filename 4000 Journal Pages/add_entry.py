873a5cc1d111c4f046b959690eb958dc5a8f07bc
Seamus Hughes <seamushughes10@gmail.com>
2025-11-09 16:16:59 +0000

    base for adding new entry
    Test

---
+++ 4000 Journal Pages/add_entry.py
@@ -0,0 +1,14 @@
+# Author: Seamus Hughes
+# Date: 9th November 2025
+# Purpose: Add additional Markdown entry for my weekly Lifestream posts.
+
+# ----Imported Modules----
+
+
+# ----Functions-----
+
+
+# ----Variables----
+
+
+# ----- 1. Setup-------
