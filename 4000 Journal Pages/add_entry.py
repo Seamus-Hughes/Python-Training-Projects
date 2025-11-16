8535b787bc0181d81d431422a54fd764e4b72b45
Seamus Hughes <seamushughes10@gmail.com>
2025-11-16 03:47:15 +0000

    create define last mod file function
    Moved discovery of last modified file into a function.

--- 4000 Journal Pages/add_entry.py
+++ 4000 Journal Pages/add_entry.py
@@ -5,10 +5,37 @@
 # ----Imported Modules----
 import os # for files
 
-from datetime import datetime  # For date management
-
 # ----Functions-----
 
+def last_mod_file(directory):
+	'''Compare all files in defined directory. Compare date last modified, returning file last modified.'''
+	
+	# Create list of files in Lifestreams folder
+	output_contents = os.listdir(directory)
+	
+	# variables to check the most recent file
+	most_recent_file = None
+	# NOTE 'None' is used to create an empty variable
+	most_recent_time = 0
+	# NOTE files will have modification time greater that 0
+	
+	# Loop through log folder
+	for item_name in output_contents:
+		# Create full path to the file
+		source_path = os.path.join(directory, item_name)
+		
+		# Check so only process files not Directories.
+		if os.path.isfile(source_path):
+			# get modification times for this file
+			source_mod_time = os.path.getmtime(source_path)
+			
+			#If this file is newer that current most recent
+			if source_mod_time > most_recent_time:
+				most_recent_time = source_mod_time
+				most_recent_file = source_path
+
+	# Return the name of file last saved	
+	return most_recent_file
 
 # ----Variables----
 log_folder = "Lifestreams"
@@ -18,27 +45,4 @@ log_folder = "Lifestreams"
 
 # ----- 2.Identify file-----
 
-
-# Create list of files in Lifestreams folder
-output_contents = os.listdir(log_folder)
-
-# variables to check the most recent file
-most_recent_file = None
-# NOTE 'None' is used to create an empty variable
-most_recent_time = 0
-# NOTE files will have modification time greater that 0
-
-# Loop through log folder
-for item_name in output_contents:
-	# Create full path to the file
-	source_path = os.path.join(log_folder, item_name)
-	
-	# Check so only process files not Directories.
-	if os.path.isfile(source_path):
-		# get modification times for this file
-		source_mod_time = os.path.getmtime(source_path)
-		
-		#If this file is newer that current most recent
-		if source_mod_time > most_recent_time:
-			most_recent_time = source_mod_time
-			most_recent_file = source_path
+print(last_mod_file(log_folder))
